e83ad6f1c84cd705114e1e88046319dc49aa1087
fridavbg <fperssontech@gmail.com>
2022-10-10 17:30:28 +0200

    Single book query

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLNonNull(GraphQLInt) },
         name: { type: GraphQLNonNull(GraphQLString) },
+        books: {
+            type: GraphQLList(BookType),
+            resolve: (author) => {
+                return books.filter((book) => book.authorId === author.id);
+            },
+        },
     }),
 });
 
@@ -57,11 +63,27 @@ const RootQueryType = new GraphQLObjectType({
     name: "Query",
     description: "Root Query",
     fields: () => ({
+        book: {
+            type: BookType,
+            description: "A single book",
+            // resolve would return from db
+            args: {
+                id: { type: GraphQLInt },
+            },
+            resolve: (parent, args) =>
+                books.find((book) => book.id === args.id),
+        },
         books: {
             type: new GraphQLList(BookType),
             description: "List of all books",
             // resolve would return from db
             resolve: () => books,
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            description: "List of all books",
+            // resolve would return from db
+            resolve: () => authors,
         },
     }),
 });
